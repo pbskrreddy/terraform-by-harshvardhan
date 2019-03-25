8807f4d7e7760ae860f9e60d1758f315b16a79a5
pbskrreddy <41994479+pbskrreddy@users.noreply.github.com>
2019-03-25 12:30:58 +0530

    terraform basics data

---
+++ test.tf
@@ -0,0 +1,34 @@
+aws_acces_key = "AFNFSJFSFJNNKSADNDF"
+
+aws_secret-key = "JFDASJFJFS;FJSJKNNFRW9RIJFOJRJ"
+
+aws_region = "us-east-1"
+
+vpc_cidr = "10.1.0.0/16"
+
+public_subnet1_cidr = "10.1.1.0/24"
+
+public_subnet2_cidr = "10.1.2.0/24"
+
+public_subnet3_cidr = "10.1.3.0/24"
+
+private_subnet_cidr = "10.1.20.0/24"
+
+vpc_name = "terraform-aws-testing"
+
+IGW_name = "terraform-aws-igw"
+
+public_subnet_name = "Terraform_Public_subnet1-testing"
+
+public_subnet_name = "Terraform_Public_subnet2-testing"
+
+public_subnet_name = "Terraform_Public_subnet3-testing"
+
+private_subnet_name = "Terraform_Private_Subnet-testing"
+
+Main_Routing_Table  = "Terraform_Main_table-testing"
+
+key_name = "linuxkeypair"
+
+environment = "dev"
+
